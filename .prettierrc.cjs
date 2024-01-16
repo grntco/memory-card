c40bc3c59d784306426ff5eee45a60180341b1a5
Grant Collins <grantcollins03@gmail.com>
2024-01-16 16:33:40 -0500

    project setup/prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+const config = {
+    trailingComma: 'all',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: true,
+}
+
+module.exports = config
