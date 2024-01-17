982818dcfc73d71d82a687f75fcead0f00843fb2
Grant Collins <grantcollins03@gmail.com>
2024-01-17 11:36:23 -0500

    added dummy card data

---
+++ src/cardData.js
@@ -0,0 +1,50 @@
+export const cardData = [
+    {
+        value: 1,
+    },
+    {
+        value: 2,
+    },
+    {
+        value: 3,
+    },
+    {
+        value: 4,
+    },
+    {
+        value: 5,
+    },
+    {
+        value: 6,
+    },
+    {
+        value: 7,
+    },
+    {
+        value: 8,
+    },
+    {
+        value: 9,
+    },
+    {
+        value: 10,
+    },
+    {
+        value: 11,
+    },
+    {
+        value: 12,
+    },
+    {
+        value: 13,
+    },
+    {
+        value: 14,
+    },
+    {
+        value: 15,
+    },
+    {
+        value: 16,
+    },
+]
