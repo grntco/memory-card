f6896607e691061d4e59416080929822b37fd961
Grant Collins <grantcollins03@gmail.com>
2024-01-17 11:28:17 -0500

    refactored shuffle to fisher yates algo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
+import { fisherYatesShuffle } from './utils/shuffle'
 
 function App() {
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    console.log(fisherYatesShuffle(array))
+
     return <></>
 }
 
--- src/utils/shuffle.js
+++ src/utils/shuffle.js
@@ -1,23 +1,10 @@
-export function shuffle(cards) {
-    const shuffledCards = []
-
-    cards.forEach((card) => {
-        card.position = _changePosition(cards.length)
-    })
-
-    for (let i = 0; i < cards.length; i++) {
-        const card = cards.find((card) => card.position === i)
-        shuffledCards.push(card)
+export function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        let temp = array[j]
+        array[j] = array[i]
+        array[i] = temp
     }
 
-    return shuffledCards
-}
-
-function _changePosition(limit, usedPositions = []) {
-    const newPosition = Math.floor(Math.random() * limit)
-    if (usedPositions.includes(newPosition)) {
-        return _changePosition(limit, usedPositions)
-    }
-    usedPositions.push(newPosition)
-    return newPosition
+    return array
 }
