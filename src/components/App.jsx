554c0cdf2bdaa5e06dbd9a197ce623def5f4fac4
Grant Collins <grantcollins03@gmail.com>
2024-01-22 12:07:58 -0500

    rn cards/setCards to deck/setDeck

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,14 @@ import { fetchCharacterData } from '../utils/fetchCharacterData'
 function App() {
     const [currentScore, setCurrentScore] = useState(0)
     const [highScore, setHighScore] = useState(0)
-    const [cards, setCards] = useState([])
+    const [deck, setDeck] = useState([])
     const [dealtCards, setDealtCards] = useState([])
     const [loaded, setLoaded] = useState(false)
 
     useEffect(() => {
         ;(async () => {
             const characterData = await fetchCharacterData(16)
-            setCards(() => {
+            setDeck(() => {
                 return characterData.map((character) => {
                     return {
                         name: character.name,
@@ -33,10 +33,10 @@ function App() {
     }, [])
 
     useEffect(() => {
-        if (cards.length > 0) {
-            setDealtCards([...deal(8, shuffle(cards))])
+        if (deck.length > 0) {
+            setDealtCards([...deal(8, shuffle(deck))])
         }
-    }, [cards])
+    }, [deck])
 
     useEffect(() => {
         setTimeout(() => {
@@ -47,7 +47,7 @@ function App() {
     return (
         <div className='app'>
             <Scoreboard
-                cards={cards}
+                cards={deck}
                 currentScore={currentScore}
                 highScore={highScore}
             />
@@ -69,7 +69,7 @@ function App() {
             setHighScore(currentScore)
         }
         setCurrentScore(0)
-        setCards((prevCards) =>
+        setDeck((prevCards) =>
             prevCards.map((card) => {
                 return { ...card, clicked: false }
             }),
@@ -79,7 +79,7 @@ function App() {
     function moveToNextPlay(card) {
         card.clicked = true
         setCurrentScore((prevCurrentScore) => prevCurrentScore + 1)
-        setCards((prevCards) => [...shuffle(prevCards)])
+        setDeck((prevCards) => [...shuffle(prevCards)])
     }
 }
 
